0feae5d18c115635ff36c5094440e3b331aec0bd
Mswork6 <denis.ch74@mail.ru>
2024-06-01 00:15:20 +0500

    feat: made new corridor generation algorithm

--- Assets/Scripts/RoomMapGenerator.cs
+++ Assets/Scripts/RoomMapGenerator.cs
@@ -1,15 +1,14 @@
 using System.Collections.Generic;
+using System.Linq;
 using Data;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 public class RoomMapGenerator : AbstractMapGenerator
 {
-    [SerializeField] private int minRoomSize = 4;
-    [SerializeField] private int maxRoomSize = 4;
+    [SerializeField] private int minRoomWidth = 4, minRoomHeight = 4;
     [SerializeField] private int mapWidth = 20, mapHeight = 20;
     [SerializeField] [Range(0, 10)] private int offset = 1;
-    [SerializeField] [Range(0, 20)] private int roomOffset = 2;
+    [SerializeField] private int roomSpacing = 1;
 
     protected override void RunProceduralGeneration()
     {
@@ -18,13 +17,9 @@ public class RoomMapGenerator : AbstractMapGenerator
 
     private void CreateRooms()
     {
-        // var roomsList = ProceduralGenerationAlgorithms.BinarySpacePartitioning(new BoundsInt((Vector3Int)startPosition,
-        //     new Vector3Int(mapWidth, mapHeight, 0)), minRoomWidth, minRoomHeight);
-        
-        var roomsList = ProceduralGenerationAlgorithms.CreateVariableSizeRooms
-        (new BoundsInt((Vector3Int)startPosition, 
-            new Vector3Int(mapWidth, mapHeight, 0)), 
-            minRoomSize, maxRoomSize, roomOffset);
+        var roomsList = ProceduralGenerationAlgorithms.CreateVariableSizeRooms(
+            new BoundsInt((Vector3Int)startPosition, new Vector3Int(mapWidth, mapHeight, 0)), 
+            minRoomWidth, minRoomHeight, roomSpacing); // Передаем зазор в метод
 
         HashSet<Vector2Int> floor = CreateSimpleRooms(roomsList);
 
@@ -47,41 +42,108 @@ public class RoomMapGenerator : AbstractMapGenerator
 
     private HashSet<Vector2Int> ConnectRooms(List<Vector2Int> roomCenters)
     {
-        HashSet<Vector2Int> corridors = new HashSet<Vector2Int>();
-        var currentRoomCenter = roomCenters[Random.Range(0, roomCenters.Count)];
-        roomCenters.Remove(currentRoomCenter);
+        List<Edge> edges = CreateAllEdges(roomCenters);
+        edges.Sort((edge1, edge2) => edge1.Weight.CompareTo(edge2.Weight));
 
-        while (roomCenters.Count > 0)
+        Dictionary<Vector2Int, Vector2Int> parent = InitializeParentDictionary(roomCenters);
+
+        HashSet<Vector2Int> corridors = new HashSet<Vector2Int>();
+
+        foreach (var edge in edges)
         {
-            Vector2Int closest = FindClosestPointTo(currentRoomCenter, roomCenters);
-            roomCenters.Remove(closest);
-            HashSet<Vector2Int> newCorridor = CreateCorridor(currentRoomCenter, closest);
-            currentRoomCenter = closest;
-            corridors.UnionWith(newCorridor);
+            if (Find(parent, edge.RoomA) != Find(parent, edge.RoomB))
+            {
+                Union(parent, edge.RoomA, edge.RoomB);
+                corridors.UnionWith(CreateCorridor(edge.RoomA, edge.RoomB));
+            }
         }
 
         return corridors;
     }
 
+    private List<Edge> CreateAllEdges(List<Vector2Int> roomCenters)
+    {
+        List<Edge> edges = new List<Edge>();
+
+        for (int i = 0; i < roomCenters.Count; i++)
+        {
+            for (int j = i + 1; j < roomCenters.Count; j++)
+            {
+                edges.Add(new Edge(roomCenters[i], roomCenters[j]));
+            }
+        }
+
+        return edges;
+    }
+
+    private Dictionary<Vector2Int, Vector2Int> InitializeParentDictionary(List<Vector2Int> roomCenters)
+    {
+        Dictionary<Vector2Int, Vector2Int> parent = new Dictionary<Vector2Int, Vector2Int>();
+        foreach (var center in roomCenters)
+        {
+            parent[center] = center;
+        }
+        return parent;
+    }
+
+    private Vector2Int Find(Dictionary<Vector2Int, Vector2Int> parent, Vector2Int room)
+    {
+        if (parent[room] != room)
+        {
+            parent[room] = Find(parent, parent[room]);
+        }
+        return parent[room];
+    }
+
+    private void Union(Dictionary<Vector2Int, Vector2Int> parent, Vector2Int roomA, Vector2Int roomB)
+    {
+        Vector2Int rootA = Find(parent, roomA);
+        Vector2Int rootB = Find(parent, roomB);
+        if (rootA != rootB)
+        {
+            parent[rootB] = rootA;
+        }
+    }
+
     private HashSet<Vector2Int> CreateCorridor(Vector2Int start, Vector2Int end)
     {
         HashSet<Vector2Int> corridor = new HashSet<Vector2Int>();
+        
         Vector2Int currentPosition = start;
-
-        while (currentPosition != end)
+        corridor.Add(currentPosition);
+        
+        while (currentPosition.x != end.x)
         {
-            if (currentPosition.x != end.x)
+            if (currentPosition.x < end.x)
             {
-                currentPosition.x += (end.x > currentPosition.x) ? 1 : -1;
+                currentPosition += Vector2Int.right;
             }
-            else if (currentPosition.y != end.y)
+            else if (currentPosition.x > end.x)
             {
-                currentPosition.y += (end.y > currentPosition.y) ? 1 : -1;
+                currentPosition += Vector2Int.left;
+            }
+            corridor.Add(currentPosition);
+        }
+        
+        while (currentPosition.y != end.y)
+        {
+            if (currentPosition.y < end.y)
+            {
+                currentPosition += Vector2Int.up;
+            }
+            else if (currentPosition.y > end.y)
+            {
+                currentPosition += Vector2Int.down;
             }
             corridor.Add(currentPosition);
         }
 
         return corridor;
+    }
+
+    private float Distance(Vector2Int a, Vector2Int b)
+    {
+        return Vector2Int.Distance(a, b);
     }
 
     private Vector2Int FindClosestPointTo(Vector2Int currentRoomCenter, List<Vector2Int> roomCenters)
@@ -118,7 +180,7 @@ public class RoomMapGenerator : AbstractMapGenerator
 
         return floor;
     }
-    
+
     private HashSet<Vector2Int> ExpandFloor(HashSet<Vector2Int> floorPositions)
     {
         HashSet<Vector2Int> wallPositions = new HashSet<Vector2Int>(floorPositions);
@@ -133,3 +195,5 @@ public class RoomMapGenerator : AbstractMapGenerator
         return wallPositions;
     }
 }
+
+
