c57e5215a4ea819f3a6304203bcae2519c48fa71
AlexCawl <babushkinsusu@gmail.com>
2024-06-02 19:37:17 +0500

    feat: added splash screen

---
+++ Assets/SplashScreenSetup.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScreenSetup : MonoBehaviour
+{
+    public void SetupPlayButton()
+    {
+        Debug.Log("TODO - PLAY");
+    }
+
+    public void SetupTutorialButton()
+    {
+        Debug.Log("TODO - TUTORIAL");
+    }
+
+    public void SetupDebugButton()
+    {
+        SceneManager.LoadSceneAsync("Scenes/SampleScene");
+    }
+}
