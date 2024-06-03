ebb5fae32c5fc32cc51386b21b9dc1d09c1d0f6a
Mswork6 <denis.ch74@mail.ru>
2024-06-04 01:00:17 +0500

    code refactoring

--- Assets/Scripts/Procedural Generation/AbstractMapGenerator.cs
+++ Assets/Scripts/Procedural Generation/AbstractMapGenerator.cs
@@ -1,14 +1,31 @@
 using UnityEngine;
+using UnityEngine.Serialization;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public abstract class AbstractMapGenerator : MonoBehaviour
 {
     [SerializeField] protected TilemapVisualizer tilemapVisualizer = null;
     [SerializeField] protected Vector2Int startPosition = Vector2Int.zero;
+    [SerializeField] protected GameObject propsContainer;
 
     public void GenerateMap()
     {
         tilemapVisualizer.Clear();
         RunProceduralGeneration();
+    }
+    
+    public void DeleteAllProps()
+    {
+        foreach (Transform child in propsContainer.transform)
+        {
+            #if UNITY_EDITOR
+            DestroyImmediate(child.gameObject);
+            #else
+            Destroy(child.gameObject);
+            #endif
+        }
     }
 
     protected abstract void RunProceduralGeneration();
