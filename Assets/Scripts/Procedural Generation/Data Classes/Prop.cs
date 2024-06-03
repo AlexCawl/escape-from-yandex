a7b7b3b474e1c8d5744d5461b49bb7d9acd264ca
Mswork6 <denis.ch74@mail.ru>
2024-06-04 02:36:28 +0500

    feat: made main part of procedure generation

--- Assets/Scripts/Procedural Generation/Data Classes/Prop.cs
+++ Assets/Scripts/Procedural Generation/Data Classes/Prop.cs
@@ -19,16 +19,8 @@ public class Prop : ScriptableObject
     public bool NearWallRight = true;
     public bool NearWallLeft = true;
     public bool Inner = true;
-    [Min(1)]
+    [Min(0)]
     public int PlacementQuantityMin = 1;
     [Min(1)]
     public int PlacementQuantityMax = 1;
-
-    [Space, Header("Group placement:")]
-    public bool PlaceAsGroup = false;
-    [Min(1)]
-    public int GroupMinCount = 1;
-    [Min(1)]
-    public int GroupMaxCount = 1;
-    
 }
